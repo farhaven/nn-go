da255e74b4afc8a1d79b0de6f5c7f899564ad9e5
Gregor Best <gbe@unobtanium.de>
2019-07-09 00:27:08 +0200

    Train net with XOR

--- network_test.go
+++ network_test.go
@@ -23,19 +23,13 @@ func TestNetworkBackprop(t *testing.T) {
 	input := []float64{0, 1}
 	target := []float64{1}
 
-	log.Println(`In:`, input, `Target:`, target)
-
 	output1 := net.forward(input)
 	error1 := net.error(output1, target)
-
-	log.Println(`Out1`, output1, `Err1`, error1)
 
 	net.backprop(input, error1, 2)
 
 	output2 := net.forward(input)
 	error2 := net.error(output2, target)
-
-	log.Println(`Out2`, output2, `Err2`, error2)
 
 	// Calculate squared errors to see if there's at least some improvement
 	se1 := float64(0)
@@ -49,5 +43,55 @@ func TestNetworkBackprop(t *testing.T) {
 
 	if se2 >= se1 {
 		t.Errorf(`backprop failed to improve error: error1: %f, error2: %f`, se1, se2)
+	}
+}
+
+func TestNetworkLearnXOR(t *testing.T) {
+	net := NewNetwork([]int{2, 3, 1})
+
+	samples := map[[2]float64][]float64 {
+		[2]float64{0, 0}: []float64{0},
+		[2]float64{0, 1}: []float64{1},
+		[2]float64{1, 0}: []float64{1},
+		[2]float64{1, 1}: []float64{0},
+	}
+
+	targetMSE := 0.005
+	learningRate := 0.1
+	iter := 0
+
+	for {
+		iter += 1
+		meanSquaredError := float64(0)
+
+		for input, target := range samples {
+			input := input[:]
+			output := net.forward(input)
+			error := net.error(output, target)
+			net.backprop(input, error, learningRate)
+
+			for _, e := range error {
+				meanSquaredError += math.Pow(e, 2)
+			}
+		}
+
+		meanSquaredError /= float64(len(samples))
+
+		// Report squared error
+		if iter % 100 == 0 {
+			log.Println(`iter`, iter, `MSE`, meanSquaredError)
+		}
+
+		if meanSquaredError <= targetMSE {
+			break
+		}
+	}
+
+	log.Println(`reached target MSE`, targetMSE, `after`, iter, `iterations`)
+
+	// Eval each sample
+	for input, target := range samples {
+		output := net.forward(input[:])
+		log.Println(`Input:`, input, `Target:`, target, `Output:`, output)
 	}
 }
