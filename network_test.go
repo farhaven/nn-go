515885b6315a567f3208755a52ca3e61ff05c228
Gregor Best <gbe@unobtanium.de>
2019-07-18 18:14:22 +0200

    Add test for the documentation example

--- network_test.go
+++ network_test.go
@@ -141,3 +141,43 @@ func TestLayerSnapshotAndRestoreNewLayer(t *testing.T) {
 		t.Errorf(`Output changed: expected %v, got %v`, output1, output2)
 	}
 }
+
+func TestDocExample(t *testing.T) {
+	type Sample [2][]float64  // Input and output
+	samples := []Sample{
+		Sample{[]float64{0, 0}, []float64{0}},
+		Sample{[]float64{0, 1}, []float64{1}},
+		Sample{[]float64{1, 0}, []float64{1}},
+		Sample{[]float64{1, 1}, []float64{0}},
+	}
+
+	learningRate := 0.75
+	targetMSE := 0.005  // Target mean squared error
+
+	net := NewNetwork([]int{2, 3, 1}, LeakyRELUActivation{Leak: 0.01})
+
+	for iter := 0; ; iter++ {
+		meanSquaredError := float64(0)
+		for _, s := range samples {
+			output := net.Forward(s[0])
+			error := net.Error(output, s[1])
+			net.Backprop(s[0], error, learningRate)
+
+			for _, e := range error {
+				meanSquaredError += math.Pow(e, 2)
+			}
+		}
+		meanSquaredError /= float64(len(samples))
+
+		if meanSquaredError <= targetMSE {
+			break
+		}
+
+		t.Log(`Iter`, iter, `MSE`, meanSquaredError)
+	}
+
+	// Print out network for each sample:
+	for i, s := range samples {
+		t.Logf(`Sample %d: %v -> %v`, i, s, net.Forward(s[0]))
+	}
+}
